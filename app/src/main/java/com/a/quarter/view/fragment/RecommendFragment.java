c99f27f75845309d2c86136d2a50676772248af3
songmofan <1915553752@qq.com>
2017-07-21 09:08:19 +0800

    Merge branch 'master' of https://github.com/QuarterFour/QuarterFourLibrary
    # Conflicts:
    #	.idea/vcs.xml

---
+++ app/src/main/java/com/a/quarter/view/fragment/RecommendFragment.java
@@ -0,0 +1,30 @@
+package com.a.quarter.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.a.quarter.R;
+
+/**
+ * 类的作用：
+ * <p>
+ * 作者： 宋莫凡
+ * <p>
+ * 思路：
+ * <p>
+ * on 2017/7/20 21
+ */
+
+public class RecommendFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.recommendfragment_layout,container,false);
+        // TODO: 2017/7/20      
+        
+    }
+}
