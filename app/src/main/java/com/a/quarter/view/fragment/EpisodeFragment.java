2a9f16f03dbcf62c23841121c07358d314ad1bc4
gitsname <gitsname>
2017-07-26 20:46:18 +0800

    Merge branch 'master' of https://github.com/QuarterFour/QuarterFourLibrary
    # Conflicts:
    #	app/src/main/res/layout/episodefragment_layout.xml

--- app/src/main/java/com/a/quarter/view/fragment/EpisodeFragment.java
+++ app/src/main/java/com/a/quarter/view/fragment/EpisodeFragment.java
@@ -4,19 +4,21 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.a.quarter.R;
 import com.a.quarter.view.adapter.MyEpisodeAdapter;
+import com.a.quarter.view.adapter.MySatinListVAdapter;
 
 import java.util.ArrayList;
+
 
 /**
  * 类的作用：段子的
@@ -39,7 +41,9 @@ public class EpisodeFragment extends Fragment implements View.OnClickListener{
     private Episode_OddphotosFragment episode_oddphotosFragment;
     private TabLayout satin_tab;
     private MyEpisodeAdapter episodeAdapter;
-
+    private MySatinListVAdapter satinListVAdapter;
+    private ListView list_view;
+    private ArrayList<String> arr;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -51,66 +55,28 @@ public class EpisodeFragment extends Fragment implements View.OnClickListener{
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        satin_tab = (TabLayout) view.findViewById(R.id.satin_tab_layout);
-        lin = (LinearLayout) view.findViewById(R.id.lin);
+
+        TextView satin_tab = (TextView) view.findViewById(R.id.satin);
+        list_view = (ListView) view.findViewById(R.id.list_view);
 
         //设置段子的两个title
-        satin_tab.addTab(satin_tab.newTab().setText("段子"));
-        satin_tab.addTab(satin_tab.newTab().setText("趣图"));
-
-        satin_tab.setTabMode(TabLayout.MODE_FIXED);
-
-        lin.setOnClickListener(this);
 
         initData();
     }
 
     private void initData() {
 
-        fragments = new ArrayList<>();
-        episode_satinFragment = new Episode_SatinFragment();
-        episode_oddphotosFragment = new Episode_OddphotosFragment();
 
-        fragments.add(episode_satinFragment);
-        fragments.add(episode_oddphotosFragment);
-        episodeAdapter = new MyEpisodeAdapter(getActivity().getSupportFragmentManager());
+        satinListVAdapter = new MySatinListVAdapter(getActivity());
 
-       setDefaultFragment();
+        arr = new ArrayList<>();
 
-        //TabLayout的监听
-        satin_tab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                FragmentTransaction transaction = getActivity()
-                        .getSupportFragmentManager()
-                        .beginTransaction();
+        list_view.setAdapter(satinListVAdapter);
 
-                transaction.replace(R.id.lin,episodeAdapter.getItem(position)).commit();
-            }
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
 
     }
 
-    //设置默认显示的Fragment
-    private void setDefaultFragment() {
-
-        FragmentTransaction transaction = getActivity()
-                .getSupportFragmentManager()
-                .beginTransaction();
-
-        transaction.replace(R.id.lin,episodeAdapter.getItem(0)).commit();
-    }
 
 
     @Override
@@ -118,9 +84,7 @@ public class EpisodeFragment extends Fragment implements View.OnClickListener{
 
         switch (v.getId()){
 
-            case R.id.lin:
-                Toast.makeText(getActivity(), "趣图", Toast.LENGTH_SHORT).show();
-                break;
+
 
         }
 
