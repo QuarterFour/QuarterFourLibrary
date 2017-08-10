58ed3d4186d924cb617cb3cf3aa983cea41448e5
gitsname <gitsname>
2017-08-10 11:02:42 +0800

    分享_qq和qq空间成功

--- app/src/main/java/com/a/quarter/system/MyAppliction.java
+++ app/src/main/java/com/a/quarter/system/MyAppliction.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.UMShareAPI;
+
 import java.util.ArrayList;
 
 /**
@@ -21,6 +24,13 @@ public class MyAppliction extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        UMShareAPI.get(this);
+
+        //微信和QQ分享
+        //PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+        PlatformConfig.setQQZone("1106163122", "aYFBhRfnHWXAkrPT");
+
 
         activities = new ArrayList<>();
         mContext=this;
